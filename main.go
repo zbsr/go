9666bd331ce58f964ef46fe3ea2b2956eff1e6ff
zhanbo <zbstayreal@icloud.com>
2018-06-14 10:50:41 +0800

    db debug

--- main.go
+++ main.go
@@ -20,7 +20,8 @@ func main()  {
 	fmt.Println("test...")
 
 	// db
-	DB, err := xorm.NewEngine("mysql", fmt.Sprintf(
+	var err error
+	DB, err = xorm.NewEngine("mysql", fmt.Sprintf(
 		`%s:%s@tcp(%s:%s)/%s?charset=utf8&loc=Asia%%2FShanghai`,
 		GetEnv("DB_USERNAME"),
 		GetEnv("DB_PASSWORD"),
@@ -29,9 +30,21 @@ func main()  {
 		GetEnv("DB_DATABASE"),
 	))
 	if err != nil {
+		fmt.Println("wrong")
 		panic(err)
 	}
+
+	DB.ShowSQL()
+	DB.ShowExecTime()
 	DB.SetLogLevel(core.LOG_DEBUG)
+	DB.IsTableExist("test")
+
+	user := make([]UserModel, 0)
+	err = DB.Find(&user)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(user)
 
 	router := gin.Default()
 
